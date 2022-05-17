c5a73670e56582a0d9ad5603174ac53f412108aa
koenbenne <koen.benne@gmail.com>
2022-05-17 14:21:35 +0200

    Very responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,8 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <div class={styles.Header}>
-      Esports Lama's
+      <img src={require('./Logo-transparent.png')} class={styles.Image}/>
+      <h1 class={styles.Title}>Esports Lama's</h1>
     </div>
   );
 }
