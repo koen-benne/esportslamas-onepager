d2cb9106eff628e33b0268855b18d8d7932318ae
Esolutions-IT <43686864+Esolutions-IT@users.noreply.github.com>
2022-05-17 15:34:11 +0200

    Insights cards
    Insights cards

--- src/components/Insights/index.js
+++ src/components/Insights/index.js
@@ -1,9 +1,53 @@
+import Container from '../Container';
 import styles from './Insights.module.css';
 
 function Insights() {
   return (
-    <div class={styles.Insights_title}>
-      Belangrijke inzichten
+    <div class={styles.Insights}>
+        <Container>
+            <div class={styles.Insights_title}>
+                Belangrijke inzichten
+            </div>
+            <div class={styles.row}>
+                <div class={styles.column}>
+                    <div class={styles.Insights_cardsItem}>
+                        <h4 class={styles.Insights_cardtitle}>Videogames verbeteren de aandacht capaciteit in het algemeen</h4>
+                        <hr class={styles.Insights_cardDivider}></hr>
+                        <p class={styles.Insights_cardSubTitle}>Desk research</p>
+                    </div>
+                </div>
+                <div class={styles.column}>
+                    
+                </div>
+                <div class={styles.column}>
+                    <div class={styles.Insights_cardsItem}>
+                        <h4 class={styles.Insights_cardtitle}>Videogames verbeteren de aandacht capaciteit in het algemeen</h4>
+                        <hr class={styles.Insights_cardDivider}></hr>
+                        <p class={styles.Insights_cardSubTitle}>Desk research</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class={styles.row}>
+                <div class={styles.column}>
+                    <div class={styles.Insights_cardsItem}>
+                        <h4 class={styles.Insights_cardtitle}>Videogames verbeteren de aandacht capaciteit in het algemeen</h4>
+                        <hr class={styles.Insights_cardDivider}></hr>
+                        <p class={styles.Insights_cardSubTitle}>Desk research</p>
+                    </div>
+                </div>
+                <div class={styles.column}>
+                    
+                </div>
+                <div class={styles.column}>
+                    <div class={styles.Insights_cardsItem}>
+                        <h4 class={styles.Insights_cardtitle}>Videogames verbeteren de aandacht capaciteit in het algemeen</h4>
+                        <hr class={styles.Insights_cardDivider}></hr>
+                        <p class={styles.Insights_cardSubTitle}>Desk research</p>
+                    </div>
+                </div>
+            </div>
+        </Container>
     </div>
   );
 }
