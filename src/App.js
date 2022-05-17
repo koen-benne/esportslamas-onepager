3794303894c71853e4993ba94d7a2863f69f0719
Rik <rik.vermeulen.1997@live.nl>
2022-05-17 13:11:28 +0200

    added target section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import TargetQuestionSection from './components/TargetQuestionSection';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <TargetQuestionSection/>
     </div>
   );
 }
